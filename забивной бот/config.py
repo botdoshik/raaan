7c251753dec74e790db7d916dff9e66e71333e5b
mcdrbogdanov <106861562+mcdrbogdanov@users.noreply.github.com>
2022-12-25 00:21:43 +0300

    Create config.py

---
+++ забивной бот/config.py
@@ -0,0 +1,5 @@
+API_TOKEN = '5781331229:AAFf_ZBvDE951OTMeJQF_t18Y-Yp4kgS4zA' # токен от тлеграм бота
+
+admin = '5348342250' # id админа, тобиж твой (взять тут - t.me/userinfobot)
+
+admin_name = '@ektreppro' # юзернейм админа, c @
