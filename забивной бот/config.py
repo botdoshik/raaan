37705643fcdf08065b9692153a22dd67b28392f7
mcdrbogdanov <106861562+mcdrbogdanov@users.noreply.github.com>
2023-01-04 07:27:11 +0300

    Update config.py

--- забивной бот/config.py
+++ забивной бот/config.py
@@ -1,4 +1,4 @@
-API_TOKEN = '5781331229:AAFf_ZBvDE951OTMeJQF_t18Y-Yp4kgS4zA' # токен от тлеграм бота
+API_TOKEN = '5548529962:AAF7LB60g_l0FiH20jRGgV20Eb1orCMejZU' # токен от тлеграм бота
 
 admin = '5348342250' # id админа, тобиж твой (взять тут - t.me/userinfobot)
 
