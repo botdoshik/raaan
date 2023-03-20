3ae169f4b2893ffd1d19d7f03b39cbedf79c8288
botdoshik <127345021+botdoshik@users.noreply.github.com>
2023-03-20 21:17:42 +0300

    Update config.py

--- забивной бот/config.py
+++ забивной бот/config.py
@@ -1,4 +1,4 @@
-API_TOKEN = '5512775097:AAH-IKHGxi45o8QmZdQs-FPU3SbWCR74Xg0' # токен от тлеграм бота
+API_TOKEN   =  ' 5497442201 : AAHLCvtgBVT9JXFw1M_S3aL0uTnLRyEnZQ0  ' # токен от тлеграм бота
 
 admin = '5348342250' # id админа, тобиж твой (взять тут - t.me/userinfobot)
 
